c95d3dad079cb9b222a9aea5cff8ae7622ed7fae
dmachine7 <60094449+dmachine7@users.noreply.github.com>
2020-11-30 09:13:22 +0530

    Updates

---
+++ precache-manifest.9ba7df66d332c3d81850550494ac40eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e97555ecea8cd4dc539d8de5f513de8",
+    "url": "/meme_generator/index.html"
+  },
+  {
+    "revision": "3de7320da889dec103eb",
+    "url": "/meme_generator/static/css/main.9c6dd986.chunk.css"
+  },
+  {
+    "revision": "88edd86e5dd70411bfb9",
+    "url": "/meme_generator/static/js/2.b081afc5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meme_generator/static/js/2.b081afc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3de7320da889dec103eb",
+    "url": "/meme_generator/static/js/main.cc9acea4.chunk.js"
+  },
+  {
+    "revision": "41193c6f355a9505b113",
+    "url": "/meme_generator/static/js/runtime-main.f29a8313.js"
+  }
+]);
